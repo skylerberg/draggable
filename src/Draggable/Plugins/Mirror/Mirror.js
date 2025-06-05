d2ccf3640b4536a80cad7fca603f429fe6e42fa0
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-06-04 21:05:02 -0700

    Do not round mirror placement
    Rounding was initially added to work around an issue in chrome that
    caused blurriness when fractional values were used in translate3d().
    However, since then, these bugs appear to have been fixed.
    
    Rounding causes the mirror to not appear in the exact position that the
    original object was. Since the values here are CSS pixels and not real
    pixels, on some devices this rounding can be more than one real pixel.

--- src/Draggable/Plugins/Mirror/Mirror.js
+++ src/Draggable/Plugins/Mirror/Mirror.js
@@ -536,17 +536,15 @@ function positionMirror({withFrame = false, initial = false} = {}) {
 
         if (mirrorOffset) {
           const x = passedThreshX
-            ? Math.round(
-                (sensorEvent.clientX - mirrorOffset.left - scrollOffset.x) /
-                  (options.thresholdX || 1),
-              ) * (options.thresholdX || 1)
-            : Math.round(lastMovedX);
+            ? ((sensorEvent.clientX - mirrorOffset.left - scrollOffset.x) /
+                (options.thresholdX || 1)) *
+                (options.thresholdX || 1)
+            : lastMovedX;
           const y = passedThreshY
-            ? Math.round(
-                (sensorEvent.clientY - mirrorOffset.top - scrollOffset.y) /
-                  (options.thresholdY || 1),
-              ) * (options.thresholdY || 1)
-            : Math.round(lastMovedY);
+            ? ((sensorEvent.clientY - mirrorOffset.top - scrollOffset.y) /
+                (options.thresholdY || 1)) *
+                (options.thresholdY || 1)
+            : lastMovedY;
 
           if ((options.xAxis && options.yAxis) || initial) {
             mirror.style.transform = `translate3d(${x}px, ${y}px, 0)`;
